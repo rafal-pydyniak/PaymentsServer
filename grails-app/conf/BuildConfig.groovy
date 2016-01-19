e7a3b3b934e292992f18fa39c92b7bc56077abe3
rafi94 <xardi55@gmail.com>
2016-01-19 18:36:22 +0100

    Fixing docs on server

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -66,7 +66,7 @@ grails.project.dependency.resolution = {
         compile ":scaffolding:2.1.2"
         compile ':cache:1.1.8'
         compile "org.grails.plugins:asset-pipeline:2.6.10"
-        
+
         // plugins needed at runtime but not for compilation
         runtime ":hibernate4:4.3.8.1" // or ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
